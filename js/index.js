53aba5492970bee591b297edfdc96a23d36e0a35
Jhones <jhonesdigno666@gmail.com>
2023-08-30 23:15:17 +0800

    Implemented nav for mobile

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    const menu_btn = document.querySelector(".menu_icon");
+    const links_container = document.querySelector(".links");
+    const links = links_container.querySelectorAll("a");
     const shorten_form = document.querySelector("#shorten_form");
     const url_field = shorten_form.querySelector("[name='url']");
     const shorten_api= "https://api.shrtco.de/v2/shorten?url=";
 
+    /* Toggle the links when menu button is clicked */
+    menu_btn.addEventListener("click", () => {
+        links_container.classList.toggle("active");
+    });
+
+    /* Toggle the links when clicked on any link */
+    for(let index = 0; index < links.length; index++){
+        links[index].addEventListener("click", () => {
+            links_container.classList.toggle("active");
+        });        
+    }
 
     shorten_form.addEventListener("click", (event) => {
         event.preventDefault();
@@ -21,7 +35,6 @@ document.addEventListener("DOMContentLoaded", () => {
             .catch(error => {
                 console.error('An error occurred:', error);
             });
-
     });
 
 })
